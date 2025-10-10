aabd53ccc26a8d4517f2df79a19f1bb66384b0e8
Kartik Dhasmana <54183908+kaydee001@users.noreply.github.com>
2025-10-10 11:20:39 +0530

    loading cifar10 dataset

--- cifar10_classifier/model.py
+++ cifar10_classifier/model.py
@@ -2,9 +2,13 @@ import torch
 import torch.nn as nn
 
 class SimpleCNN(nn.Module):
-    def __init__():
+    def __init__(self):
         super(SimpleCNN, self).__init__()
-        pass
+        
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1) # input channels (3), output channels (16), 3x3 filter, padding=1         
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2) # 2x2 pooling which reduces the size by half
+        
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1) # input channels (16), output channels (32), 3x3 filter, padding=1
 
     def forward(self, x):
         pass
--- cifar10_classifier/train.py
+++ cifar10_classifier/train.py
@@ -6,10 +6,22 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f'using device : {device}')
+# print(f'using device : {device}')
 
 def main():
-    pass
+    # converting image to tensor and then normalizing the rgb channels
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    
+    # loading the data set
+    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    
+    # produces batch of size = 4 images, 3 input channels -> RGB, 32x32 pixels
+    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+
+    # converting the train_loader into an iterator (the 1st batch only)
+    images, labels = next(iter(train_loader))
+    print(f"batch shape : {images.shape}")
+    print(f"labels : {labels}")
 
 if __name__ == "__main__":
-    print("cifar10 classifier")
+    main()
